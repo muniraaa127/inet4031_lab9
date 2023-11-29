d4d3e8cfa034918c740daad33033e186b3112503
Munira Ahmed <muniraahmed1368@gmail.com>
2023-11-29 21:35:53 +0000

    first commit

---
+++ lamp_stack_server.pp
@@ -0,0 +1,60 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure => installed,
+  notify  => Service['apache2'],
+  require => [Package['apache2']],
+}
+file { '/var/www/html/phpinfo.php':
+  source => '/home/muniraa127/munira_inet4031_puppet_lab9/phpinfo.php',
+  notify  => Service['apache2'],
+  require => [Package['apache2']]
+
+}
+
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => [Package['apache2'], Package['php']]
+}
+
+package { 'libapache2-mod-php':
+  ensure => installed,
+  require => [Package['php']],
+}
+
+package { 'php-cli':
+  ensure => installed,
+  require => [Package['php']],
+}
+
+package { 'php-mysql':
+  ensure => installed,
+  require => [Package['php']],
+}
+
+package { 'mysql-server':
+  ensure => installed,
+}
+
+service { 'mysql':
+  ensure => running,
+  enable => true,
+  require => [Package['mysql-server']],
+}
+
+package { 'mariadb-server':
+  ensure => installed,
+}
+
+service { 'mariadb':
+  ensure => running,
+  enable => true,
+  require => [Package['mariadb-server']],
+}
+
+
+
